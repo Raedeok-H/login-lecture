77557ea68e6caf3a4813b5be75144f7cfddb9268
readeokhwang <raedeokhwang@gmail.com>
2023-09-17 20:16:09 +0900

    돔을 이용하여 html 객체 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,17 @@
 "use strict"
 
-console.log("hello");
-console.log("hbye");
+
+// DOM: Document Object Model -> js에서 html에 존재하는 데이터들을 가져올 수 있음
+const id = document.querySelector("#id"), //#은 id를 의미, #id는 id가 id인것
+    psword = document.querySelector("#psword"),
+    loginBtn = document.querySelector("button");
+
+loginBtn.addEventListener("click", login);
+
+function login(){
+    const req = {
+        id: id.value,
+        psword: psword.value
+    };
+    console.log(req);
+}
